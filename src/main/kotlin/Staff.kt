e87db6d40651c2dabad0778d31b9b88c00c89e44
Muhammad Hassan Raza <raihassanraza10@gmail.com>
2023-09-20 16:18:38 +0500

    Added remaining source files

---
+++ src/main/kotlin/Staff.kt
@@ -0,0 +1,20 @@
+open class Staff(
+    val Emp_ID: String,
+    val Emp_Role: String,
+    val Emp_Dept: String,
+    val Emp_Salary: Float,
+    val Emp_Experience: Int
+) : Person() {
+
+    fun Attend(duty: String): String {
+        return "You successfully attended the $duty duty"
+    }
+
+    fun getPromotion(): String {
+        return "Congrats! You are now promoted <3"
+    }
+
+    fun getSalary(): Float {
+        return Emp_Salary
+    }
+}
